9d2d81fca6d8f828693c780dbc38f2ba0d9b4bcd
Jake Lehle, Ph.D. <84940857+JakeLehle@users.noreply.github.com>
2025-03-26 13:34:14 -0500

    v2.1.2
    Fixed issue #21 with not being able to download resource dir for first-time users

--- cello/download_resources.py
+++ cello/download_resources.py
@@ -23,7 +23,7 @@ def download(dest):
         'curl https://deweylab.biostat.wisc.edu/cell_type_classification/resources_v2.0.0.tar.gz > {}'.format(
             join(dest, 'resources_v2.0.0.tar.gz')
         ),
-        'tar -C {} -zxf resources_v2.0.0.tar.gz'.format(dest),
+        'tar -zxf {}'.format(join(dest, 'resources_v2.0.0.tar.gz')),
         'rm {}'.format(join(dest, 'resources_v2.0.0.tar.gz'))
     ]
     for cmd in cmds:
